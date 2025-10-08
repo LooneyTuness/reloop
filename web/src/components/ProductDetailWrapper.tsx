505b4b37e466e7f0304f538311c71c1b16f1917e
Filip <filip.panoski@hotmail.com>
2025-10-08 13:38:37 +0200

    Fix cart state issue: Remove duplicate CartProvider from ProductDetailWrapper

--- web/src/components/ProductDetailWrapper.tsx
+++ web/src/components/ProductDetailWrapper.tsx
@@ -1,12 +1,7 @@
 // src/components/ProductDetailWrapper.tsx
 "use client";
 import ProductDetail from "./ProductDetail";
-import { CartProvider } from "@/contexts/CartContext";
 
 export default function ProductDetailWrapper() {
-  return (
-    <CartProvider>
-      <ProductDetail />
-    </CartProvider>
-  );
+  return <ProductDetail />;
 }
